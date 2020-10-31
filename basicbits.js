1e0758aec3653f3b47bc9a50b123fba2c1dc1735
Zamfir Andrei Paul <zamfir.andreipaul@yahoo.com>
2020-10-31 23:05:30 +0200

    Resolve Bugs

--- basicbits.js
+++ basicbits.js
@@ -1,3 +1,28 @@
+var viewer;
+var options = {
+    'env' : 'Local',
+    'document' : './House/f0224dd3-8767-45c1-ff99-5c9c881b9fee/0.svf'
+};
+
+var config3d = {
+    extensions: ['MyAwesomeExtension']
+};
+
+Autodesk.Viewing.Initializer(options, function() {
+
+    var htmlDiv = document.getElementById('MyViewerDiv');
+    viewer = new Autodesk.Viewing.GuiViewer3D(htmlDiv, config3d);
+    var startedCode = viewer.start(options.document, options);
+    if (startedCode > 0) {
+        console.error('Failed to create a Viewer: WebGL not supported.');
+        return;
+    }
+
+    console.log('Initialization complete, loading a model next...');
+
+});
+
+
 function MyAwesomeExtension(viewer, options) {
     Autodesk.Viewing.Extension.call(this, viewer, options);
   }
@@ -6,14 +31,39 @@ function MyAwesomeExtension(viewer, options) {
   MyAwesomeExtension.prototype.constructor = MyAwesomeExtension;
   
   MyAwesomeExtension.prototype.load = function() {
-    
     alert('MyAwesomeExtension is loaded!');
+    this.onSelectionBinded = this.onSelectionEvent.bind(this);
+    this.viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, this.onSelectionBinded);
     return true;
   };
-  /*
-  function userFunction(pdb) {
-    
-    //return 42;
+  
+  MyAwesomeExtension.prototype.unload = function() {
+    alert('MyAwesomeExtension is now unloaded!');
+    return true;
+  };
+  var currSelection;
+  MyAwesomeExtension.prototype.onSelectionEvent = function(event) {
+    currSelection = this.viewer.getSelection();
+    console.log(currSelection);
+    var thePromise = viewer.model.getPropertyDb().executeUserFunction( userFunction );
+    thePromise.then(function(retValue){
+        console.log('retValue is: ', retValue); // prints 'retValue is: 42'
+    }).catch(function(err){
+    console.log("Something didn't go right...")
+    console.log(err);
+    });
+ };
+
+    MyAwesomeExtension.prototype.load = function() {
+        this.onSelectionBinded = this.onSelectionEvent.bind(this);
+        this.viewer.addEventListener(Autodesk.Viewing.SELECTION_CHANGED_EVENT, this.onSelectionBinded);
+        return true;
+    };
+
+    function userFunction(pdb) {
+        
+        //return 42;
+
     var attrIdMass = -1;
 
     // Iterate over all attributes and find the index to the one we are interested in
@@ -28,86 +78,36 @@ function MyAwesomeExtension(viewer, options) {
     });
 
     // Early return is the model doesn't contain data for "Mass".
-    if (attrIdMass === -1)
-      return null;
 
     // Now iterate over all parts to find out which one is the largest.
     var maxValue = 0;
     var maxValId = -1;
-    pdb.enumObjects(function(dbId){
+    
+    //pdb.enumObjects(function(dbId){
 
         // For each part, iterate over their properties.
-        pdb.enumObjectProperties(dbId, function(attrId, valId){
+        pdb.enumObjectProperties(2834, function(attrId, valId){
+            console.log(attrId+valId);
 
             // Only process 'Mass' property.
-            // The word "Property" and "Attribute" are used interchangeably.
-            if (attrId === attrIdMass) {
+            // The word "Property" and "Attribute" are used interchangeably
 
-                var value = pdb.getAttrValue(attrId, valId);
-
-                if (value > maxValue) {
-                    maxValue = value;
-                    maxValId = dbId;
-                }
-
-                // Stop iterating over additional properties when "Mass" is found.
-                return true;
-            }
+            var value = pdb.getAttrValue(attrId, valId);
+            console.log(value);
+            // Stop iterating over additional properties when "Mass" is found.
         });
-    });
+    //});
 
     // Return results
     return {
       id: maxValId,
       mass: maxValue
     }
-  }
-  var thePromise = viewer.model.getPropertyDb().executeUserFunction( userFunction );
-thePromise.then(function(retValue){
 
-    //if (retValue === 42) {
-    //  console.log('We got the expected value back.');
-    //}
-
-    if (!retValue) {
-      console.log("Model doesn't contain property 'Mass'.");
-      return;
     }
-
-    var mostMassiveId = retValue.id;
-    viewer.select(mostMassiveId);
-    viewer.fitToView([mostMassiveId]);
-    console.log('Most massive part is', mostMassiveId, 'with Mass:', retValue.mass);
-});
-  this.eventTool = new EventTool(this.viewer)
-
-    this.eventTool.on('singleclick', (event) => {
-      const hitTest = this.viewer.clientToWorld(
-        event.canvasX, event.canvasY, true)
-
-      if (hitTest) {
-        const it = this.viewer.model.getData().instanceTree
-
-        const nodeName = it.getNodeName(hitTest.dbId)
-
-        if ((/^.*(floor).*$/gi).test(nodeName)) {
-          this.faderCore.hitTest = hitTest
-
-          this.update()
-        }
-      }
-    })*/
-  MyAwesomeExtension.prototype.unload = function() {
-    alert('MyAwesomeExtension is now unloaded!');
-    return true;
+  
+  document.getElementById("test1").onclick = function(){
+    
   };
 
-  function test(){
-    let viewer;
-    let viewerDiv = document.getElementById('MyViewerDiv');
-    viewer = new Autodesk.Viewing.Private.GuiViewer3D(viewerDiv);
-    var DBids = viewer.impl.selector.getAggregateSelection(); // Special case to handle multi-model selection
-    DBids = DBids.map( a=> {return a.selection});  // filter out the model objects
-    console.log(JSON.stringify(DBids));
-    }
-  Autodesk.Viewing.theExtensionManager.registerExtension('BasicBits', MyAwesomeExtension);
+  Autodesk.Viewing.theExtensionManager.registerExtension('MyAwesomeExtension', MyAwesomeExtension);
