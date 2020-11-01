66cd05df1a287503013c9370fe7d9f6bac077bb2
Electricity2015 <andrei.boy2014@gmail.com>
2020-11-01 14:35:14 +0200

    PDF bug

--- makepdf.js
+++ makepdf.js
@@ -12,53 +12,84 @@ document.getElementById("makepdftest").onclick = function(){
     });
     var textcereretipArray = [];
     var textmediuArray = [];
-    var textcereretipArray = [];
-    var textcereretipArray = [];
+    var textsanatateArray = [];
+    var textapeleRoamneArray = [];
+    var textisuArray = [];
     var line=10;
     doc.text("Aviz: ", 20, line);
     line+=30;
-    var i=0;
+    var i=0;var j=0;var k=0; var l=0;var m=0;
     doc.setFont('times','normal');
-    if(cereretipArray && cereretipArray.length){
+    var check1=(cereretipArray && cereretipArray.length);
+    var check2=(mediuArray && mediuArray.length);
+    var check3=(sanatateArray && sanatateArray.length);
+    var check4=(apeleRoamneArray && apeleRoamneArray.length);
+    var check5=(isuArray && isuArray.length);
+    var status=1;
+    if(check1)
+    {
+        status=0;
         for (i in cereretipArray){
             textcereretipArray.push(cereretipArray[i].name + '  ' + cereretipArrayDescription[i])
         }
-        for (j in mediuArray){
-            textmediuArray.push(mediuArrayy[j].name + '  ' + mediuArrayDescription[j])
-        }
-        for (k in cereretipArray){
-            textcereretipArray.push(cereretipArray[k].name + '  ' + cereretipArrayDescription[k])
-        }
-        for (l in cereretipArray){
-            textcereretipArray.push(cereretipArray[l].name + '  ' + cereretipArrayDescription[l])
-        }
-        for (m in cereretipArray){
-            textcereretipArray.push(cereretipArray[m].name + '  ' + cereretipArrayDescription[m])
-        }
-        
-        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
+        doc.text("Probleme Aviz privind energia electrica: ", 20, line=line+10);
         doc.text(textcereretipArray, 20, line=line+10);
-        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
-        doc.text(text, 20, line=line+10);
-        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
-        doc.text(text, 20, line=line+10);
-        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
-        doc.text(text, 20, line=line+10);
-        doc.text('Status: ', 20, line=line+10);
-        doc.setTextColor(255,0,0);
-        doc.text('Dezaprobat ', 20, line=line+10);
-        doc.setTextColor(255,255,255);
-    }else{
-        doc.text("Fara probleme", 20, line=line+10);
         line= line+i*10;
+    }
+    if(check2)
+    {
+        status=0;
+        for (j in mediuArray){
+            textmediuArray.push(mediuArray[j].name + '  ' + mediuArrayDescription[j])
+        }
+        doc.text("Probleme Aviz de mediu: ", 20, line=line+10);
+        doc.text(textmediuArray, 20, line=line+10);
+        line= line+j*10;
+    }
+    if(check3)
+    {
+        status=0;
+        for (k in apeleRoamneArray){
+            textapeleRoamneArray.push(apeleRoamneArray[k].name + '  ' + apeleRoamneDescription[k])
+        }
+        doc.text("Probleme Aviz de la apele romane: ", 20, line=line+10);
+        doc.text(textapeleRoamneArray, 20, line=line+10);
+        line= line+k*10;
+    }
+    if(check4)
+    {
+        status=0;
+        for (l in sanatateArray){
+            textsanatateArray.push(sanatateArray[l].name + '  ' + sanatateArray[l])
+        }
+        doc.text("Probleme Aviz sanatatea populatiei: ", 20, line=line+10);
+        doc.text(textsanatateArray, 20, line=line+10);
+        line= line+l*10;
+    }
+    if(check5)
+    {
+        status=0;
+        for (m in isuArray){
+            textisuArray.push(isuArray[m].name + '  ' + isuArrayDescription[m])
+        }
+        doc.text("Probleme Aviz ISU: ", 20, line=line+10);
+        doc.text(textisuArray, 20, line=line+10);
+        line= line+m*10;
+    }
+    if(status)
+    {
+        doc.text("Fara probleme", 20, line=line+10);
         doc.text('Status: ', 20, line=line+10);
         doc.setTextColor(0,255,0);
         doc.text('Aprobat ', 20, line=line+10);
         doc.setTextColor(255,255,255);
     }
-    
+    else{
+        doc.text('Status: ', 20, line=line+10);
+        doc.setTextColor(255,0,0);
+        doc.text('Dezaprobat ', 20, line=line+10);
+        doc.setTextColor(255,255,255);
+    }
     doc.save('a4.pdf');
     console.log("PDF Creat");
 };
-    
-  
