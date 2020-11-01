e523935646f3afc9e52f6c84b313cbf7f9a0d2de
Electricity2015 <andrei.boy2014@gmail.com>
2020-11-01 12:59:28 +0200

    last min changes

--- makepdf.js
+++ makepdf.js
@@ -10,17 +10,39 @@ document.getElementById("makepdftest").onclick = function(){
         format: 'a4',
         putOnlyUsedFonts:true 
     });
-    var text = [];
+    var textcereretipArray = [];
+    var textmediuArray = [];
+    var textcereretipArray = [];
+    var textcereretipArray = [];
     var line=10;
     doc.text("Aviz: ", 20, line);
     line+=30;
     var i=0;
+    doc.setFont('times','normal');
     if(cereretipArray && cereretipArray.length){
         for (i in cereretipArray){
-            text.push(cereretipArray[i].name + '  ' + cereretipArrayDescription[i])
+            textcereretipArray.push(cereretipArray[i].name + '  ' + cereretipArrayDescription[i])
         }
-        doc.text("Probleme: ", 20, line);
-        doc.setFont('times','normal');
+        for (j in mediuArray){
+            textmediuArray.push(mediuArrayy[j].name + '  ' + mediuArrayDescription[j])
+        }
+        for (k in cereretipArray){
+            textcereretipArray.push(cereretipArray[k].name + '  ' + cereretipArrayDescription[k])
+        }
+        for (l in cereretipArray){
+            textcereretipArray.push(cereretipArray[l].name + '  ' + cereretipArrayDescription[l])
+        }
+        for (m in cereretipArray){
+            textcereretipArray.push(cereretipArray[m].name + '  ' + cereretipArrayDescription[m])
+        }
+        
+        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
+        doc.text(textcereretipArray, 20, line=line+10);
+        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
+        doc.text(text, 20, line=line+10);
+        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
+        doc.text(text, 20, line=line+10);
+        doc.text("Probleme Aviz privind energia electrica: ", 20, line);
         doc.text(text, 20, line=line+10);
         doc.text('Status: ', 20, line=line+10);
         doc.setTextColor(255,0,0);
