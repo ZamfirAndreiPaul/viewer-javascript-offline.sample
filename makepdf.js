4106b84bda73c86005d006d34f71e24a27dd54be
Electricity2015 <andrei.boy2014@gmail.com>
2020-11-01 03:21:07 +0200

    PDF #2

--- makepdf.js
+++ makepdf.js
@@ -1,16 +1,20 @@
-
-import { jsPDF } from "jspdf";
-
+const { jsPDF } = window.jspdf;
 
 document.getElementById("makepdftest").onclick = function(){
     
-var doc = new jsPDF()
+    var doc = new jsPDF()
 
-doc.text('Hello world!', 10, 10)
-doc.save('a4.pdf')
+    doc.text(20, 20, 'This is the default font.')
+    doc.setFont('courier','normal')
+    doc.text(20, 30, 'This is courier normal.')
+    doc.setFont('times','italic')
+    doc.text(20, 40, 'This is times italic.')
+    doc.setFont('helvetica','bold')
+    doc.text(20, 50, 'This is helvetica bold.')
+    doc.setFont('courier','bolditalic')
+    doc.text(20, 60, 'This is courier bolditalic.')    
+    doc.save('a4.pdf')
     console.log("PDF Creat");
 };
+    
   
-  
-  
-  //};
