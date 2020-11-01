2ed3abbfc4dead508185fc56152d2343d2ea26d6
Zamfir Andrei Paul <zamfir.andreipaul@yahoo.com>
2020-11-01 03:21:59 +0200

    Add table at buttons

--- basicbits.js
+++ basicbits.js
@@ -66,6 +66,35 @@
         console.log(pdb.getObjectProperties(userData[0]));
         return pdb.getObjectProperties(userData[0]);
     }
+
+    MyAwesomeExtension.prototype.onToolbarCreated = function(toolbar) {
+        // alert('TODO: customize Viewer toolbar');
+      
+        var viewer = this.viewer;
+      
+        // Button 1
+        var button1 = new Autodesk.Viewing.UI.Button('show-env-bg-button');
+        button1.onClick = function(e) {
+            viewer.setEnvMapBackground(true);
+        };
+        button1.addClass('show-env-bg-button');
+        button1.setToolTip('Show Environment');
+      
+        // Button 2
+        var button2 = new Autodesk.Viewing.UI.Button('hide-env-bg-button');
+        button2.onClick = function(e) {
+            viewer.setEnvMapBackground(false);
+        };
+        button2.addClass('hide-env-bg-button');
+        button2.setToolTip('Hide Environment');
+      
+        // SubToolbar
+        this.subToolbar = new Autodesk.Viewing.UI.ControlGroup('my-custom-toolbar');
+        this.subToolbar.addControl(button1);
+        this.subToolbar.addControl(button2);
+      
+        toolbar.addControl(this.subToolbar);
+    };
   
   document.getElementById("test1").onclick = function(){
     console.log(currentItem);
